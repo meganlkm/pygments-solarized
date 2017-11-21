42b0e32c18f0b6f9997404303bfb2b9e8c7e59da
Megan <megan.lkm@gmail.com>
2017-11-21 09:49:58 -0600

    fix imports

--- pygments_solarized/__init__.py
+++ pygments_solarized/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
-from .light import SolarizedStyle
-from .dark import SolarizedDarkStyle
-from .dark256 import SolarizedDark256Style
+from pygments_solarized.light import SolarizedStyle
+from pygments_solarized.dark import SolarizedDarkStyle
+from pygments_solarized.dark256 import SolarizedDark256Style
 
-__version__ = '0.0.2'
+__version__ = '0.0.3'
